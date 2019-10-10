ffc5c65d84e8325519a80e826dd70f6329212eb2
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-10 13:01:34 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  if num / 3
+  if num / 5
     return "Fizz"
-  elsif num / 5
+  elsif num / 3
     puts "Buzz"
   elsif num / 3 || num / 5
     puts "FizzBuzz"
