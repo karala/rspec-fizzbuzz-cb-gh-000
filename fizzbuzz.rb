e6cfc5ab205b44b2716c77e320f7a097a8c0ba0b
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-10 13:01:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   if num / 3
-    returns "Fizz"
+    return "Fizz"
   elsif num / 5
     puts "Buzz"
   elsif num / 3 || num / 5
